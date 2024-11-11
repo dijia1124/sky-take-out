7fa202ad1763a0c72f38fcf9aaff089fc5226f6e
dijia1124 <zrh19981123@gmail.com>
2024-11-11 16:38:31 +1100

    Implement redis cache clearance when necessary

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -10,9 +10,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -22,6 +24,8 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * Add dish
@@ -34,6 +38,9 @@ public class DishController {
         log.info("Add dish：{}", dishDTO);
 
         dishService.saveWithFlavor(dishDTO);
+
+        // clear redis cache
+        clearRedisCache("dish_" + dishDTO.getCategoryId());
         return Result.success();
     }
 
@@ -61,6 +68,8 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("Delete dish：{}", ids);
 
+        // clear redis cache by pattern: all of dishes
+        clearRedisCache("dish_*");
         dishService.deleteBatch(ids);
         return Result.success();
     }
@@ -88,6 +97,8 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO){
         log.info("Update dish：{}", dishDTO);
 
+        // clear redis cache
+        clearRedisCache("dish_*");
         dishService.updateWithFlavor(dishDTO);
         return Result.success();
     }
@@ -100,10 +111,32 @@ public class DishController {
     @GetMapping("/list")
     @ApiOperation("Get dish by category id")
     public Result<List<DishVO>> list(Long categoryId){
-        log.info("Get dish list by cat id：{}", categoryId);
 
+        // construct a key
+        String key = "dish_" + categoryId;
+
+        // if the key exists in redis, return the value without querying the database
+        if (Boolean.TRUE.equals(redisTemplate.hasKey(key))) {
+            if (redisTemplate.opsForValue().get(key) == null) {
+                return Result.success(null);
+            }
+            List<DishVO> dishVOList = (List<DishVO>) redisTemplate.opsForValue().get(key);
+            return Result.success(dishVOList);
+        }
+        // if the key does not exist in redis, query the database
         List<DishVO> dishVOList = dishService.listByCategoryId(categoryId);
+        // store the result in redis
+        redisTemplate.opsForValue().set(key, dishVOList);
         return Result.success(dishVOList);
     }
+
+    /**
+     * clear redis cache
+     * @param pattern
+     */
+    private void clearRedisCache(String pattern) {
+        Set keys = redisTemplate.keys(pattern);
+    }
+    // todo: missing status on/off method
 
 }
