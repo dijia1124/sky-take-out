b21ee8f85c2a49221c21540682c4dc1ee6a3a226
dijia1124 <zrh19981123@gmail.com>
2024-11-11 15:34:24 +1100

    separate user and admin doc

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -21,7 +21,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import java.util.List;
 
 /**
- * 配置类，注册web层相关组件
+ * configuration class for web mvc
  */
 @Configuration
 @Slf4j
@@ -31,27 +31,27 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
 
     /**
-     * 注册自定义拦截器
+     * register custom interceptor
      *
      * @param registry
      */
     protected void addInterceptors(InterceptorRegistry registry) {
-        log.info("开始注册自定义拦截器...");
+        log.info("registering custom interceptor...");
         registry.addInterceptor(jwtTokenAdminInterceptor)
                 .addPathPatterns("/admin/**")
                 .excludePathPatterns("/admin/employee/login");
     }
 
     /**
-     * 通过knife4j生成接口文档
+     * generate interface doc via knife4j
      * @return
      */
     @Bean
     public Docket docket() {
         ApiInfo apiInfo = new ApiInfoBuilder()
-                .title("苍穹外卖项目接口文档")
+                .title("sky takeout project interface document")
                 .version("2.0")
-                .description("苍穹外卖项目接口文档")
+                .description("sky takeout project interface document")
                 .build();
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo)
@@ -61,6 +61,44 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
                 .build();
         return docket;
     }
+
+    @Bean
+    public Docket docket1() {
+        log.info("create docket1...");
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("sky takeout project interface document")
+                .version("2.0")
+                .description("sky takeout project interface document")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("admin interface")
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sky.controller.admin"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+
+    @Bean
+    public Docket docket2() {
+        log.info("create docket2...");
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("sky takeout project interface document")
+                .version("2.0")
+                .description("sky takeout project interface document")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("user interface")
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sky.controller.user"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+
+
 
     /**
      * 设置静态资源映射
