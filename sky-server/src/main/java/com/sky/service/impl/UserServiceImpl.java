f1a5cde7b2a9b6068fe3b829b9822f386b298456
dijia1124 <zrh19981123@gmail.com>
2024-11-12 12:59:39 +1100

    Fix: incorrect param in doc

--- sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -32,7 +32,7 @@ public class UserServiceImpl implements UserService {
 
     /**
      * WeChat login
-     * @param UserLoginDTO
+     * @param userLoginDTO
      * @return
      */
     public User wxLogin(UserLoginDTO userLoginDTO) {
